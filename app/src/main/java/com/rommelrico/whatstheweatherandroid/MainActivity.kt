2deb0b16bb325f4564926f5dcd9dab9cf248324a
Rommel Rico <me@rommelrico.com>
2018-11-12 08:40:36 -0800

    Placeholder for onClick

--- app/src/main/java/com/rommelrico/whatstheweatherandroid/MainActivity.kt
+++ app/src/main/java/com/rommelrico/whatstheweatherandroid/MainActivity.kt
@@ -2,11 +2,16 @@ package com.rommelrico.whatstheweatherandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun getWeather(view: View) {
+        // TODO: Implement me.
     }
 }
