9f8b44cfa734f74674156c4d1190e91b87910367
Rommel Rico <me@rommelrico.com>
2018-11-12 08:49:52 -0800

    Object properties

--- app/src/main/java/com/rommelrico/whatstheweatherandroid/MainActivity.kt
+++ app/src/main/java/com/rommelrico/whatstheweatherandroid/MainActivity.kt
@@ -3,8 +3,13 @@ package com.rommelrico.whatstheweatherandroid
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var editText: EditText? = null
+    var resultTextView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
